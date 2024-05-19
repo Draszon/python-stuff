05e4bf8842d7f5513b25092ee5cfa2582390cc80
Draszon <peti0202@windowslive.com>
2024-05-19 12:11:43 +0200

    Bug fix

--- darts.py
+++ darts.py
@@ -14,39 +14,54 @@ playerTwoScore = gameType
 def scoreCalculator(playerName, playerScore):
   print("\n{} jön, {}-pontnál jársz!".format(playerName, playerScore))
   throwCounter = 1
+  currentTotal = 0
   while throwCounter <= 3:
     currentThrow = input("{}. dobás eredménye: ".format(throwCounter))
 
-    while not currentThrow: #and currentThrow.startswith("d") and currentThrow.startswith("t"):
+    while not currentThrow:
       currentThrow = input("{}. dobás eredménye: ".format(throwCounter))
 
     if currentThrow.startswith('d'):
       currentThrow = int(currentThrow[1:]) * 2
+      currentTotal += currentThrow
+      throwCounter += 1
+      if playerScore - currentTotal < 0:
+        break
     elif currentThrow.startswith('t'):
       currentThrow = int(currentThrow[1:]) * 3
+      currentTotal += currentThrow
+      throwCounter += 1
+      if playerScore - currentTotal < 0:
+        break
     else:
       currentThrow = int(currentThrow)
-
-    if playerScore - currentThrow < 0:
-      break
-    elif playerScore - currentThrow > 0:
-      playerScore -= currentThrow
+      currentTotal += currentThrow
       throwCounter += 1
-    elif playerScore - currentThrow == 0:
-      playerScore -= currentThrow
-      return playerScore
-    else:
-      print("Valami hiba történt!")
-  print("{} pontod maradt!".format(playerScore))
-  return playerScore
+      if playerScore - currentTotal < 0:
+        break
+
+  if playerScore - currentTotal < 0:
+    return playerScore
+  elif playerScore - currentTotal >= 0:
+    return playerScore - currentTotal
+  else:
+    print("Valami hiba történt!")
 
 while playerOneScore != 0 and playerTwoScore != 0:
   playerOneScore = scoreCalculator(playerOne, playerOneScore)
   if playerOneScore == 0:
     print("Gratulálok {}! Megnyerted a játékot!".format(playerOne))
     break
+  elif playerOneScore > 0:
+    print("{} pontod maradt!".format(playerOneScore))
+  else:
+    print("Valami hiba történt!")
 
   playerTwoScore = scoreCalculator(playerTwo, playerTwoScore)
   if playerTwoScore == 0:
     print("Gratulálok {}! Megnyerted a játékot!".format(playerTwo))
     break
+  elif playerOneScore > 0:
+    print("{} pontod maradt!".format(playerTwoScore))
+  else:
+    print("Valami hiba történt!")
